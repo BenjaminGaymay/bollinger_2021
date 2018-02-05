7c254a8c5963e9578381c331e3457b9d00272af3
Benoit Pingris <benoit.pingris@epitech.eu>
2018-02-05 11:54:13 +0100

    whaou laide

--- main.py
+++ main.py
@@ -17,11 +17,24 @@ def get_info(file):
     return array
 
 
+def print_help():
+    """ Display help and exit """
+    print("""Bollinger Bands
+USAGE
+./bollinger [-h] period standard_dev indexes_file index_number
+period          number of indexes for the moving average
+standard_dev    standard deviation coefficient to apply
+indexes_file    file containing daily indexes
+index_number    index number to compute moving average and Bollinger bands
+OPTIONS
+-h              print the usage and quit.""")
+
 def main():
     """ Fonction main """
 
     array = get_info(sys.argv[1])
     if array is None:
+        print_help()
         return False
     return True
 
