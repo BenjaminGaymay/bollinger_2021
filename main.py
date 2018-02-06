e8841cc724d7c138726d2d3fc5fa073b56dc2ecd
Benoit Pingris <benoit.pingris@epitech.eu>
2018-02-06 10:33:18 +0100

    chap

--- main.py
+++ main.py
@@ -36,11 +36,9 @@ def get_file(file):
 
     try:
         with open(file, "r") as file_fd:
-            content = file_fd.read()
     except IOError:
         print("Error: File '%s' doesn't exist." % file, file=sys.stderr)
         return None
-
     return content
 
 
